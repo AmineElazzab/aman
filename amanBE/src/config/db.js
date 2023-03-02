fcb968de9c9949c0302655767bb0575ceabdc247
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-02 14:45:30 +0100

    database configiration

---
+++ amanBE/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require ("colors")
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+
+module.exports = connectDB;
+
+mongoose.set("strictQuery", false);
