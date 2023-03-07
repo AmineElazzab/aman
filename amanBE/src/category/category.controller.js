27d68383f830fad30c605f35c3764228afaa591e
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-07 21:30:41 +0100

    Category controller

---
+++ amanBE/src/category/category.controller.js
@@ -0,0 +1,82 @@
+const Category= require('./category.model')
+const asyncHandler = require('express-async-handler')
+
+
+//get all categories
+const GetCategories = asyncHandler(async (req, res) => {
+    const categories = await Category.find({})
+    res.json(categories)
+}
+);
+
+//get category by id
+const GetCategoryById = asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (category) {
+        res.json(category)
+    }
+    else {
+        res.status(404)
+        throw new Error('Category not found')
+    }
+}
+);
+
+//add category 
+const AddCategory = asyncHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+        const category = await Category.create({
+            name,
+        })
+        res.json(category)
+    }
+    catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+    }
+}
+);
+
+//update category
+const UpdateCategory = asyncHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+        const category = await Category.findByIdAndUpdate(req.params.id, {
+            name,
+        })
+        if (category) {
+            res.json(category)
+        }
+        else {
+            res.status(404)
+            throw new Error('Category not found')
+        }
+    }
+    catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+    }
+}
+);
+
+//delete category
+const DeleteCategory = asyncHandler(async (req, res) => {
+    const category = await Category.findByIdAndDelete(req.params.id)
+    if (category) {
+        res.json(category)
+    }
+    else {
+        res.status(404)
+        throw new Error('Category not found')
+    }
+}
+);
+
+module.exports = {
+    GetCategories,
+    GetCategoryById,
+    AddCategory,
+    UpdateCategory,
+    DeleteCategory
+}
