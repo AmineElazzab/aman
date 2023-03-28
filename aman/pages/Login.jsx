2cfcf33e0502a90afaee62c12b4e99d7fd3925da
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-28 14:46:59 +0000

    update login button

--- aman/pages/Login.jsx
+++ aman/pages/Login.jsx
@@ -1,95 +1,139 @@
-import { View, Text, TouchableOpacity, TextInput, StyleSheet,Image } from 'react-native'
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import m from '../assets/img/login.jpg'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Image,
+  Alert,
+  Pressable
+} from 'react-native';
+import React, {useContext, useState} from 'react';
+import m from '../assets/img/login.jpg';
+import {Login} from '../utils/api';
+import {useMutation} from 'react-query';
+import {CredentialsContext} from '../components/CredentialsContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
+export default function LoginSecrin({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {storedCredentials, setStoredCredentials} = useContext(CredentialsContext);
+
+  const {mutate, isLoadig} = useMutation(Login, {
+    onSuccess: data => {
+      persisLogin(data);
+      navigation.navigate('Home');
+    },
+    onError: error => {
+      Alert.alert('Error', error.response.data.message);
+    },
+    isLoadig: setTimeout(() => {
+      return isLoadig;
+    }, 1000),
+  });
+
+  const persisLogin = async data => {
+    AsyncStorage.setItem('token', JSON.stringify(data.token))
+    AsyncStorage.setItem('user', JSON.stringify(data.fullName))
+      .then(() => {
+        setStoredCredentials(data.token);
+        setStoredCredentials(data.fullName);
+        console.log(data.fullName);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
 
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          marginLeft: 10,
+        }}>
+        <Text
+          style={{
+            color: '#112B54',
+            fontSize: 30,
+            fontWeight: 'bold',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}>
+          Login
+        </Text>
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 15,
+            fontWeight: 'bold',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            marginTop: 15,
+            maxWidth: '60%',
+          }}>
+          Welcome Back you've been missed!
+        </Text>
+      </View>
+      <Image
+        source={m}
+        style={{
+          width: 350,
+          height: 350,
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#aaaaaa"
+        onChangeText={email => setEmail(email)}
+        value={email}
+        underlineColorAndroid="transparent"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <TextInput
+        style={styles.input}
+        placeholderTextColor="#aaaaaa"
+        secureTextEntry
+        placeholder="Password"
+        onChangeText={password => setPassword(password)}
+        value={password}
+        underlineColorAndroid="transparent"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Pressable
+        onPress={() => mutate({email, password})}
+        style={styles.button}>
+        <Icon
+          name="finger-print-outline"
+          size={30}
+          color="white"
+          style={{marginRight: 10}}
 
-const Login = () => {
-    const navigation = useNavigation();
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-
-    
-
-    return (
-        <View style={styles.container}>
-            <View
-                style={{
-                    marginLeft: 10,
-                }}
-            >
-                <Text
-                    style={{
-                        color: '#112B54',
-                        fontSize: 30,
-                        fontWeight: 'bold',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                    }}
-                >
-                    Login
-                </Text>
-                <Text
-                    style={{
-                        color: 'black',
-                        fontSize: 15,
-                        fontWeight: 'bold',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                        marginTop: 15,
-                        maxWidth: "60%"
-                    }}
-                >
-                    Welcome Back you've been missed!
-                </Text>
-            </View>
-                <Image source={m} style={{ 
-                width: 350,
-                height: 350,
-                }} />
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                placeholderTextColor="#aaaaaa"
-                onChangeText={(email) => setEmail(email)}
-                value={email}
-                underlineColorAndroid="transparent"
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            <TextInput
-                style={styles.input}
-                placeholderTextColor="#aaaaaa"
-                secureTextEntry
-                placeholder="Password"
-                onChangeText={(password) => setPassword(password)}
-                value={password}
-                underlineColorAndroid="transparent"
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            <TouchableOpacity
-                onPress={() => 
-                    navigation.navigate('Home')}
-                style={styles.button}
-            >
-                <Text style={styles.buttonTitle}>Login</Text>
-            </TouchableOpacity>
-            <View style={styles.footerView}>
-                <Text style={styles.footerText}>Don't have an account? <Text onPress={() => navigation.navigate('Register')} style={styles.footerLink}>Sign up</Text></Text>
-                {/* <Text
+        />
+      </Pressable>
+      <View style={styles.footerView}>
+        <Text style={styles.footerText}>
+          Don't have an account?{' '}
+          <Text
+            onPress={() => navigation.navigate('Register')}
+            style={styles.footerLink}>
+            Sign up
+          </Text>
+        </Text>
+        {/* <Text
                     onPress={() => onResetPasswordPress()} style={styles.footerLink}>Forgot Password?</Text> */}
-            </View>
-        </View>
-    )
+      </View>
+    </View>
+  );
 }
-
 
 // const onLoginPress = () => {
 //     setLoading(true)
@@ -117,70 +161,65 @@ const Login = () => {
 //         })
 // }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
-        backgroundColor: '#fff',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 30,
+  },
+  input: {
+    height: 50,
+    width: 350,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#F0F0F0',
+    marginBottom: 10,
+    padding: 15,
+  },
+  button: {
+    backgroundColor: '#112B54',
+    width: 350,
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    shadowColor: '#112B54',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    title: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        color: 'black',
-        marginBottom: 30,
-    },
-    input: {
-        height: 50,
-        width: 350,
-        borderRadius: 10,
-        overflow: 'hidden',
-        backgroundColor: '#F0F0F0',
-        marginBottom: 10,
-        padding: 15,
-    },
-    button: {
-        backgroundColor: '#112B54',
-        width: 350,
-        height: 50,
-        borderRadius: 10,
-        alignItems: "center",
-        justifyContent: 'center',
-        marginBottom: 10,
-        shadowColor:'#112B54',
-        shadowOffset: {
-          width: 0,
-          height: 5,
-        },
-        shadowOpacity: 0.34,
-        shadowRadius: 6.27,
-        elevation: 10,
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: 'black'
-    },
-    footerLink: {
-        color: "#112B54",
-        fontWeight: "bold",
-        fontSize: 16,
-        shadowColor: '#000',
-        elevation: 50,
-
-    }
-})
-
-export default Login
-
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  footerView: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  footerLink: {
+    color: '#112B54',
+    fontWeight: 'bold',
+    fontSize: 16,
+    shadowColor: '#000',
+    elevation: 50,
+  },
+});
