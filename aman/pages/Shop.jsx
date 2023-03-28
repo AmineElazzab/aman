7e0e410dc4447631bc6e78c4b10e79ef711d1c88
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-28 14:51:35 +0000

    display all Products

--- aman/pages/Shop.jsx
+++ aman/pages/Shop.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import Product from '../components/Product';
 
 const Shop = () => {
   return (
-    <View>
-      <Text>Shop</Text>
+    <View
+      style={{
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        paddingHorizontal: 5,
+      }}>
+      <Product />
     </View>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
