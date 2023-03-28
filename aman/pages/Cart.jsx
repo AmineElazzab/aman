711c8e2ac8ed61a4aea2cce055028e148794ddb3
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-28 14:41:54 +0000

    adding cart page but not working yet

---
+++ aman/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <View>
+      <Text>Cart</Text>
+    </View>
+  )
+}
+
+export default Cart
