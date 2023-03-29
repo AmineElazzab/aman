2d1faef25a49110f560eeb7b94b6628679f2dae3
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-29 15:55:34 +0000

    add API for Cart and Adresse

--- aman/utils/api.js
+++ aman/utils/api.js
@@ -43,3 +43,23 @@ export async function getProducts() {
     const response = await axios.get(`${URL}/api/product/getProducts`);
     return response.data;
 }
+
+export async function getProductsByCategory(categoryId) {
+    const response = await axios.get(`${URL}/api/product/getProductsByCategory/${categoryId}`);
+    return response.data;
+}
+
+export async function getProductById(productId) {
+    const response = await axios.get(`${URL}/api/product/getProductById/${productId}`);
+    return response.data;
+}
+
+export async function getCartItems() {
+    const response = await axios.get(`${URL}/api/cart/getCartItems`);
+    return response.data;
+}
+
+export async function addToCart(productId) {
+    const response = await axios.post(`${URL}/api/cart/addToCart/${productId}`);
+    return response.data;
+}
