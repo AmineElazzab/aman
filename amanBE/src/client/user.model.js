b138649b71c4e6a38f0a08e5bde44dc043627178
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-29 15:56:59 +0000

    delete adresse

--- amanBE/src/client/user.model.js
+++ amanBE/src/client/user.model.js
@@ -18,17 +18,7 @@ const userSchema = mongoose.Schema(
         {
             type: String,
             required: true
-        },
-        address:
-        {
-            type: String,
-            required: true
-        },
-        city:
-        {
-            type: String,
-            required: true
-        },
+        }
     },
     {
         timestamps: true
