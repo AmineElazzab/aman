ad96b4cd0c260902ae4be62d2b3feea006cc575f
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-04 11:21:41 +0000

    Show all products

--- aman/pages/Shop.jsx
+++ aman/pages/Shop.jsx
@@ -1,18 +1,45 @@
+// import Product from '../components/Product';
+import Category from '../components/Category';
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {getProductsByCategory} from '../utils/api';
+import {useQuery} from 'react-query';
 import Product from '../components/Product';
 
 const Shop = () => {
+  // const {categoryId} = route.params;
+  // const [category, setCategory] = useState(null);
+
+  // const {data, isLoading, error} = useQuery(
+  //   ['products', categoryId],
+  //   () => getProductsByCategory(categoryId),
+  //   {
+  //     onSuccess: data => {
+  //       setCategory(data);
+  //     },
+  //   },
+  // );
+
+  // if (isLoading) {
+  //   return <Text>Loading...</Text>;
+  // }
+  // if (error) {
+  //   return <Text>Error: {error.message}</Text>;
+  // }
+
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        width: '100%',
-        alignItems: 'center',
-        paddingHorizontal: 5,
-      }}>
-      <Product />
-    </View>
+    <>
+      {/* <Category categoryId={categoryId} /> */}
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          paddingHorizontal: 5,
+        }}>
+        <Product />
+      </View>
+    </>
   );
 };
 
