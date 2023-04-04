68236989181241ee71b923611a59faaf3b53eb8b
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-04 11:24:47 +0000

    jwt middleware

--- amanBE/src/middleware/jwt.middleware.js
+++ amanBE/src/middleware/jwt.middleware.js
@@ -1,23 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {
-    try{
-        const token = req.header.authorization.split(" ")[1];
-        const isCustomAuth = token.length < 500;
+const auth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
 
-        let decodedData;
-
-        if(token && isCustomAuth){
-            decodedData = jwt.verify(token, process.env.JWT_SECRET);
-            req.userId = decodedData?.id;
-        }else{
-            decodedData = jwt.decode(token);
-            req.userId = decodedData?.sub;
-        }
-        next();
-    }catch(error){
-        console.log(error);
+    if (!authHeader) {
+      throw new Error("Authorization header missing");
     }
+
+    const token = authHeader.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = {auth};
+module.exports = { auth };
