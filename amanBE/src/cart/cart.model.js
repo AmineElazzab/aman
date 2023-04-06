f5909df8c227b1dc55d6ae335aa67d59b0848918
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-06 14:16:05 +0000

    amanBE/src/cart/cart.model.js

--- amanBE/src/cart/cart.model.js
+++ amanBE/src/cart/cart.model.js
@@ -15,11 +15,11 @@ const cartSchema = mongoose.Schema(
             ref: 'Product',
             required: true
         },
-        quantity:
-        {
-            type: Number,
-            required: true
-        },
+        // quantity:
+        // {
+        //     type: Number,
+        //     required: true
+        // },
     },
     {
         timestamps: true
