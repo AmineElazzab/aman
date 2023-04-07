9b4da3acc25e19fc84c94e762a5755310621564d
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-07 16:17:55 +0000

    product controller

--- amanBE/src/product/product.controller.js
+++ amanBE/src/product/product.controller.js
@@ -27,7 +27,8 @@ const GetProductById = asyncHandler(async (req, res) => {
 
 //get products by category
 const GetProductsByCategory = asyncHandler(async (req, res) => {
-    const products = await Product.find({ category: req.params.id 
+    const products = await Product.find({
+        category: req.params.id
     }).populate('category');
     if (products) {
         res.json(products);
@@ -45,52 +46,39 @@ const AddProduct = asyncHandler(async (req, res) => {
         const {
             name,
             price,
-            // image,
+            image,
             description,
-            category_id,
-            countInStock
+            category,
+            // countInStock
         } = req.body;
-
-        if (
-            !name ||
-            !price ||
-            // !image ||
-            !description ||
-            !category_id ||
-            !countInStock
-        ) {
+        if (!req.file) {
             res.status(400);
-            throw new Error('Please provide all fields');
+            throw new Error('No file uploaded');
         }
-
-        const newproduct = new
-            Product({
-                name,
-                price,
-                // image,
-                description,
-                category: category_id,
-                countInStock
-            });
-        const product = await Product.findOne({
+        const { file } = req;
+        const newproduct = await Product.create({
             name,
             price,
-            // image,
+            image: file.path || null,
             description,
-            category_id,
-            countInStock
+            category: category,
+            // countInStock
         });
-        if (product) {
-            res.status(400).send({
-                success: false,
-                message: "Product already exists"
+        if (newproduct) {
+            res.status(201).json({
+                _id: newproduct._id,
+                name: newproduct.name,
+                price: newproduct.price,
+                image: newproduct.image,
+                description: newproduct.description,
+                category: newproduct.category,
+                // countInStock: newproduct.countInStock,
             });
         }
-        await newproduct.save();
-        res.status(201).send({
-            success: true,
-            message: "Product added successfully"
-        });
+        else {
+            res.status(400);
+            throw new Error('Invalid product data');
+        }
     }
     catch (error) {
         res.status(500).send({
@@ -100,84 +88,112 @@ const AddProduct = asyncHandler(async (req, res) => {
     }
 }
 );
+        //         const product = await Product.findOne({
+        //             name,
+        //             price,
+        //             image,
+        //             description,
+        //             category_id,
+        //             countInStock
+        //         });
+        //         if (product) {
+        //             res.status(400).send({
+        //                 success: false,
+        //                 message: "Product already exists"
+        //             });
+        //         }
+        //         await newproduct.save();
+        //         res.status(201).send({
+        //             success: true,
+        //             message: "Product added successfully"
+        //         });
+        //     }
+        //     catch (error) {
+        //         res.status(500).send({
+        //             success: false,
+        //             message: error.message
+        //         });
+        //     }
+        // }
+        // );
 
 
 
-//update product
-const UpdateProduct = asyncHandler(async (req, res) => {
-    const {
-        name,
-        price,
-        image,
-        description,
-        category,
-        countInStock
-    } = req.body;
-    if (
-        !name ||
-        !price ||
-        !image ||
-        !description ||
-        !category ||
-        !countInStock
-    ) {
-        res.status(400);
-        throw new Error('Please provide all fields');
-    }
+        //update product
+        const UpdateProduct = asyncHandler(async (req, res) => {
+            const {
+                name,
+                price,
+                image,
+                description,
+                category,
+                countInStock
+            } = req.body;
+            if (
+                !name ||
+                !price ||
+                !image ||
+                !description ||
+                !category ||
+                !countInStock
+            ) {
+                res.status(400);
+                throw new Error('Please provide all fields');
+            }
 
-    const product = await Product.findById(req.params.id);
+            const product = await Product.findById(req.params.id);
 
-    if (product) {
-        product.name = name;
-        product.price = price;
-        product.image = image;
-        product.description = description;
-        product.category = category;
-        product.countInStock = countInStock;
+            if (product) {
+                product.name = name;
+                product.price = price;
+                product.image = image;
+                product.description = description;
+                product.category = category;
+                product.countInStock = countInStock;
 
-        const updatedProduct = await product.save();
-        res.json({
-            _id: updatedProduct._id,
-            name: updatedProduct.name,
-            price: updatedProduct.price,
-            image: updatedProduct.image,
-            description: updatedProduct.description,
-            category: updatedProduct.category,
-            countInStock: updatedProduct.countInStock,
-            message: "Product updated successfully"
-        });
-    }
-    else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-}
-);
+                const updatedProduct = await product.save();
+                res.json({
+                    _id: updatedProduct._id,
+                    name: updatedProduct.name,
+                    price: updatedProduct.price,
+                    image: updatedProduct.image,
+                    description: updatedProduct.description,
+                    category: updatedProduct.category,
+                    countInStock: updatedProduct.countInStock,
+                    message: "Product updated successfully"
+                });
+            }
+            else {
+                res.status(404);
+                throw new Error('Product not found');
+            }
+        }
+        );
 
-//delete product
-const DeleteProduct = asyncHandler(async (req, res) => {
-    const product = await Product.findByIdAndDelete
-        (req.params.id);
-    if (product) {
-        res.json({
-            message: "Product deleted successfully"
-        });
-    }
-    else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-}
-);
+        //delete product
+        const DeleteProduct = asyncHandler(async (req, res) => {
+            const product = await Product.findByIdAndDelete
+                (req.params.id);
+            if (product) {
+                res.json({
+                    message: "Product deleted successfully"
+                });
+            }
+            else {
+                res.status(404);
+                throw new Error('Product not found');
+            }
+        }
+        );
 
-module.exports = {
-    GetProducts,
-    GetProductById,
-    GetProductsByCategory,
-    AddProduct,
-    UpdateProduct,
-    DeleteProduct
-};
+        module.exports = {
+            GetProducts,
+            GetProductById,
+            GetProductsByCategory,
+            AddProduct,
+            UpdateProduct,
+            DeleteProduct
+        };
 
 
 
