28552e5e930de34d4dc8d6100f65087f1077f633
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-07 16:18:32 +0000

    order model

--- amanBE/src/order/order.model.js
+++ amanBE/src/order/order.model.js
@@ -3,10 +3,10 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const orderSchema = mongoose.Schema(
     {
-        orderItems:
-        {
-            type: Array,
-        },
+        // orderItems:
+        // {
+        //     type: Array,
+        // },
         user:
         {
             type: mongoose.Schema.ObjectId,
@@ -19,122 +19,25 @@ const orderSchema = mongoose.Schema(
             ref: 'Product',
             required: true
         },
-        // orderItems:
-        // [
-        //     {
-        //         name:
-        //         {
-        //             type: String,
-        //             required: true
-        //         },
-        //         qty:
-        //         {
-        //             type: Number,
-        //             required: true
-        //         },
-        //         image:
-        //         {
-        //             type: String,
-        //             required: true
-        //         },
-        //         price:
-        //         {   
-        //             type: Number,
-        //             required: true
-        //         },
-        //         product:
-        //         {
-        //             type: mongoose.Schema.ObjectId,
-        //             ref: 'Product',
-        //             required: true
-        //         },
-        //     }
-        // ],
-        // shippingAddress:
-        // {
-        //     address:
-        //     {
-        //         type: String,
-        //         required: true
-        //     },
-        //     city:
-        //     {
-        //         type: String,
-        //         required: true
-        //     },
-        //     postalCode:
-        //     {
-        //         type: String,
-        //         required: true
-        //     },
-        //     country:
-        //     {
-        //         type: String,
-        //         required: true
-        //     },
-        // },
-        // paymentMethod:
-        // {   
-        //     type: String,
-        //     required: true
-        // },
-        // paymentResult:
-        // {
-        //     id:
-        //     {
-        //         type: String
-        //     },
-        //     status:
-        //     {
-        //         type: String
-        //     },
-        //     update_time:
-        //     {
-        //         type: String
-        //     },
-        //     email_address:
-        //     {
-        //         type: String
-        //     },
-        // },
-        // taxPrice:
-        // {
-        //     type: Number,
-        //     required: true,
-        //     default: 0.0
-        // },
-        // shippingPrice:
-        // {
-        //     type: Number,
-        //     required: true,
-        //     default: 0.0
-        // },
-        // totalPrice:
-        // {
-        //     type: Number,
-        //     required: true,
-        //     default: 0.0
-        // },
-        // isPaid:
-        // {
-        //     type: Boolean,
-        //     required: true,
-        //     default: false
-        // },  
-        // paidAt:
-        // {
-        //     type: Date
-        // },
-        // isDelivered:
-        // {
-        //     type: Boolean,
-        //     required: true,
-        //     default: false
-        // },
-        // deliveredAt:
-        // {
-        //     type: Date
-        // },
+        quantity:
+        {
+            type: Number,
+            required: true
+        },
+        shippingAddress:
+        {
+            type: String,
+            required: true
+        },
+        city:{
+            type: String,
+            required: true
+        },
+        postalCode:{
+            type: String,
+            required: true
+        },
+       
     },
     {
         timestamps: true
