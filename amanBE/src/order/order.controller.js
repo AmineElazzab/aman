f8c027e446d30e3ab38a860498572157c0d97899
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-07 16:18:52 +0000

    order controller

--- amanBE/src/order/order.controller.js
+++ amanBE/src/order/order.controller.js
@@ -1,4 +1,6 @@
-const User = require('../user/user.model');
+const User = require('../client/user.model');
+const Product = require('../product/product.model');
+const Cart = require('../cart/cart.model');
 const Order = require('./order.model');
 const asyncHandler = require('express-async-handler');
 
@@ -24,7 +26,10 @@ const GetOrderById = asyncHandler(async (req, res) => {
 
 //get order by user id
 const GetOrderByUserId = asyncHandler(async (req, res) => {
-    const order = await Order.find({ user: req.params.id });
+    const user = req.params.id
+    const order = await Order.find({user}).populate(
+        'product'
+    )
     if (order) {
         res.json(order);
     }
@@ -35,7 +40,46 @@ const GetOrderByUserId = asyncHandler(async (req, res) => {
 }
 );
 
-//add order 
+//add order by user id and product id
 const AddOrder = asyncHandler(async (req, res) => {
-    
-})
+    const cartId = req.params.id;
+    const cart = await Cart.findById(cartId)
+    if (cart) {
+        const order = await Order.create({
+            user: req.userId,
+            cart: cartId,
+            product: cart.product,
+            quantity: req.body.quantity,
+            shippingAddress: req.body.shippingAddress,
+            city:req.body.city,
+            postalCode: req.body.postalCode,
+        });
+        res.json(order);
+    }
+    else {
+        res.status(404);
+        throw new Error('Cart not found');
+    }
+}
+);
+
+//delete order by id
+const DeleteOrder = asyncHandler(async (req, res) => {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (order) {
+        res.json(order);
+    }
+    else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+}
+);
+
+module.exports = {
+    GetOrders,
+    GetOrderById,
+    GetOrderByUserId,
+    AddOrder,
+    DeleteOrder
+};
