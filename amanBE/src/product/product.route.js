61a3068a9b120a3550d165e694b17f8dd0ff22c4
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-07 16:17:07 +0000

    product router

--- amanBE/src/product/product.route.js
+++ amanBE/src/product/product.route.js
@@ -8,14 +8,39 @@ const {
         UpdateProduct,
         DeleteProduct,
 }= require("./product.controller")
+const multer = require('multer')
 const {auth} = require('../middleware/jwt.middleware')
+
+
+//multer configuration
+const storage = multer.diskStorage({
+ 
+     
+
+        destination: function (req, file, cb) {
+            cb(null, 'src/uploads/'); // define the destination folder for uploaded files
+    
+        },
+    
+        filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            // cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop()) // generate unique file name
+            const fileName =  file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop();// generate unique file name
+    
+            const filePath = fileName.replace(/\\/g, "/"); // enlever le backslash dans le nom de fichier
+    
+            cb(null, filePath);
+        }
+    
+    });
+const upload = multer({ storage: storage }).single('file');
 
 
 router.get("/getProducts", GetProducts);
 router.get("/getProductById/:id", GetProductById);
-router.post("/addProduct", AddProduct);
+router.post("/addProduct", upload, AddProduct);
 router.get("/getProductsByCategory/:id", GetProductsByCategory);
-router.put("/updateProduct/:id", UpdateProduct);
+router.put("/updateProduct/:id",auth, UpdateProduct);
 router.delete("/deleteProduct/:id", DeleteProduct);
 
 module.exports = router;
