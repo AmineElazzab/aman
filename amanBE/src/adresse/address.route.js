2833366248ce37957094739b4f590287669a8e8b
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-07 16:20:05 +0000

    adress route

--- amanBE/src/adresse/address.route.js
+++ amanBE/src/adresse/address.route.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const {
     AddAddress,
     GetAllAddresses,
-    GetAddressById,
+    // GetAddressById,
     GetAddressByUserId,
     UpdateAddress,
     DeleteAddress
 }= require("./address.controller");
 const {auth} = require ('../middleware/jwt.middleware')
+
+
 router.post("/addAddress", auth ,AddAddress);
 router.get("/getAllAddresses", GetAllAddresses);
-router.get("/getAddressById/:id", GetAddressById);
-router.get("/getAddressByUserId/:id", GetAddressByUserId);
-router.put("/updateAddress/:id", UpdateAddress);
-router.delete("/deleteAddress/:id", DeleteAddress);
+// router.get("/getAddressById/:id",auth, GetAddressById);
+router.get("/getAddressByUserId",auth, GetAddressByUserId);
+router.put("/updateAddress/:id",auth, UpdateAddress);
+router.delete("/deleteAddress/:id",auth, DeleteAddress);
 
 module.exports = router;
