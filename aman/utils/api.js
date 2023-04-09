81ade07d879dde1b3cc0c8f0baa53ea0c16728bd
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-09 16:12:49 +0000

    api

--- aman/utils/api.js
+++ aman/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const URL = 'http://192.168.9.20:5000';
+const URL = 'http://192.168.1.104:5000';
 
 
 export async function Login({
@@ -49,17 +50,92 @@ export async function getProductsByCategory(categoryId) {
     return response.data;
 }
 
-export async function getProductById(productId) {
-    const response = await axios.get(`${URL}/api/product/getProductById/${productId}`);
-    return response.data;
-}
-
 export async function getCartItems() {
-    const response = await axios.get(`${URL}/api/cart/getCartItems`);
+    const token = await AsyncStorage.getItem("token")
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDA3NjE1ZTcyNTVmNDAzNDNiOTA3NiIsImlhdCI6MTY4MDYxNzU2NywiZXhwIjoxNjgzMjA5NTY3fQ.OtQZdMHq7PJFjO0t-dVuB5wrARC19FnQ_vSLKrzKV-M"
+    const response = await axios.get(`${URL}/api/cart/getCartItems`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    );
+
     return response.data;
 }
 
 export async function addToCart(productId) {
-    const response = await axios.post(`${URL}/api/cart/addToCart/${productId}`);
+    const token = await AsyncStorage.getItem("token")
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDA3NjE1ZTcyNTVmNDAzNDNiOTA3NiIsImlhdCI6MTY4MDYxNzU2NywiZXhwIjoxNjgzMjA5NTY3fQ.OtQZdMHq7PJFjO0t-dVuB5wrARC19FnQ_vSLKrzKV-M"
+    const response = await axios.post(`${URL}/api/cart/addToCart/${productId}`, {}, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
+
+export async function addAddress(userId) {
+    const token = await AsyncStorage.getItem("token")
+
+    const response = await axios.post(`${URL}/api/address/addAddress/${userId}`, {}, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+
+
+    // const response = await axios.post(`${URL}/api/address/addAddress/${userId}`);
+    // return response.data;
+}
+
+export async function GetProductById(productId) {
+    const response = await axios.get(`${URL}/api/product/getProductById/${productId}`);
+    return response.data;
+}
+
+export async function DeleteCartItem(
+    cartItemId
+) {
+    const token = await AsyncStorage.getItem("token")
+    const response = await axios.delete(`${URL}/api/cart/deleteCartItem/${cartItemId}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
+
+export async function getOrderByUserId() {
+    const token = await AsyncStorage.getItem("token")
+    const response = await axios.get(`${URL}/api/order/getOrderByUserId`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
+export async function addOrder() {
+    const token = await AsyncStorage.getItem("token")
+    const response = await axios.post(`${URL}/api/order/addOrder`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
+}
+
+
+//getUserById
+export async function getUserById(userId) {
+    const token = await AsyncStorage.getItem("token")
+    const response = await axios.get(`${URL}/api/user/getUserById/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
