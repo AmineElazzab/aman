c9d48a05273bbd44bde5418464616c58da7760fa
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-04-09 15:55:36 +0000

    Chekout

---
+++ aman/components/Chekout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Chekout = () => {
+  return (
+    <View>
+      <Text>Chekout</Text>
+    </View>
+  )
+}
+
+export default Chekout
